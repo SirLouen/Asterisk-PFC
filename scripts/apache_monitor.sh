9278d9dec95b3e41af8d358d1b278c25458aa64b
Manuel Camargo <sir.louen@gmail.com>
2012-05-28 17:07:19 +0200

    nuevas modificaciones

--- scripts/apache_monitor.sh
+++ scripts/apache_monitor.sh
@@ -2,12 +2,13 @@
 
 SERVER="home-asterisk.local"
 PORT="80"
+WORKDIR="/home/asterisk/repositorio/scripts"
 
 echo "quit" | telnet $SERVER $PORT | grep "Escape character is"
 
 if [ "$?" -ne 0 ]; then
-  cp apache_warning.call apache_warning_temp.call
-  mv apache_warning_temp.call /var/spool/asterisk/outgoing/
+  cp $WORKDIR/apache_warning.call $WORKDIR/apache_warning_temp.call
+  mv $WORKDIR/apache_warning_temp.call /var/spool/asterisk/outgoing/
   exit 1
 fi
 
