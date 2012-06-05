afabc703266a8a81f38b149ac4fad8beeb0e554c
Manuel Camargo <sir.louen@gmail.com>
2012-06-05 16:54:55 +0200

    Ejemplos para BBDD

---
+++ scripts/ejemplo_pares_sip.sql
@@ -0,0 +1,29 @@
+INSERT INTO `asterisk`.`pares_sip` (`id` ,`name` ,`host` ,`nat` ,`type` ,
+`callerid`,`context`,`defaultip` ,`dtmfmode` ,`fromuser` ,`fromdomain` ,
+`insecure` ,`mailbox`,`md5secret` ,`deny` ,`permit` ,`qualify` ,`secret` ,
+`disallow` ,`allow` ,`port` ,`regserver` ,`regseconds` ,`lastms` ,`username` ,
+`defaultuser` ,`subscribecontext` ,`useragent` ,`useradmin`)
+VALUES (NULL , 'proveedorip', 'home-asterisk.local', 'no', 'friend', NULL , 
+'entrantes', NULL , NULL , 'ucaautos', NULL , 'invite', NULL , NULL , NULL , 
+NULL , 'yes', '12345678', 'all', 'g729;ilbc;gsm;ulaw;alaw', '35060', NULL , 
+'0', '0', '', 'ucaautos', NULL , NULL , '0');
+
+INSERT INTO `asterisk`.`pares_sip` (`id`, `name`, `host`, `nat`, `type`, 
+`callerid`, `context`, `defaultip`, `dtmfmode`, `fromuser`, `fromdomain`, 
+`insecure`, `mailbox`, `md5secret`, `deny`, `permit`, `qualify`, `secret`, 
+`disallow`, `allow`, `port`, `regserver`, `regseconds`, `lastms`, `username`, 
+`defaultuser`, `subscribecontext`, `useragent`, `useradmin`) 
+VALUES (NULL, '21', 'dynamic', 'no', 'friend', NULL, 'manager', NULL, 
+'rfc2833', NULL, NULL, NULL, '21@default', '5dbe664c6d998967ad9bfe4ea37521a1', 
+NULL, NULL, 'yes', '', 'all', 'g729;ilbc;gsm;ulaw;alaw', '0', NULL, '0', 
+'0', '', '', NULL, NULL, '0');
+
+INSERT INTO `asterisk`.`pares_sip` (`id`, `name`, `host`, `nat`, `type`, 
+`callerid`, `context`, `defaultip`, `dtmfmode`, `fromuser`, `fromdomain`, 
+`insecure`, `mailbox`, `md5secret`, `deny`, `permit`, `qualify`, `secret`, 
+`disallow`, `allow`, `port`, `regserver`, `regseconds`, `lastms`, `username`, 
+`defaultuser`, `subscribecontext`, `useragent`, `useradmin`) 
+VALUES (NULL, '54', 'dynamic', 'no', 'friend', NULL, 'resto', NULL, 
+'rfc2833', NULL, NULL, NULL, '54@default', '037f3c092ae05f73054d2d2dd0e7d5dc', 
+NULL, NULL, 'yes', '', 'all', 'g729;ilbc;gsm;ulaw;alaw', '0', NULL, '0', 
+'0', '', '', NULL, NULL, '1');
---
+++ scripts/usuarioasterisk.sql
@@ -0,0 +1,3 @@
+CREATE USER 'asterisk'@'localhost' IDENTIFIED BY 'asterisk'; GRANT USAGE ON * . * TO 'asterisk'@'localhost' IDENTIFIED BY 'asterisk' 
+WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
+GRANT ALL PRIVILEGES ON `asterisk` . * TO 'asterisk'@'localhost';
