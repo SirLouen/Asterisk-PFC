3cd0b271df08ca5c04c912e126c080fde5bd342f
Manuel Camargo <sir.louen@gmail.com>
2012-06-06 14:38:28 +0200

    Memoria para el sistema CDR

--- scripts/ejemplo_pares_sip.sql
+++ scripts/ejemplo_pares_sip.sql
@@ -14,7 +14,8 @@ INSERT INTO `asterisk`.`pares_sip` (`id`, `name`, `host`, `nat`, `type`,
 `disallow`, `allow`, `port`, `regserver`, `regseconds`, `lastms`, 
 `username`, `defaultuser`, `subscribecontext`, `useragent`, `useradmin`) 
 VALUES (NULL, '21', 'dynamic', 'no', 'friend', NULL, 'manager', NULL,
-'rfc2833', NULL, NULL, NULL, '21@default', '5dbe664c6d998967ad9bfe4ea37521a1', 
+'rfc2833', NULL, NULL, NULL, '21@default', 
+'5dbe664c6d998967ad9bfe4ea37521a1', 
 NULL, NULL, 'yes', '', 'all', 'g729;ilbc;gsm;ulaw;alaw', '0', NULL, '0', 
 '0', '', '', NULL, NULL, '0');
 
@@ -24,6 +25,7 @@ INSERT INTO `asterisk`.`pares_sip` (`id`, `name`, `host`, `nat`, `type`,
 `disallow`, `allow`, `port`, `regserver`, `regseconds`, `lastms`, 
 `username`, `defaultuser`, `subscribecontext`, `useragent`, `useradmin`) 
 VALUES (NULL, '54', 'dynamic', 'no', 'friend', NULL, 'resto', NULL, 
-'rfc2833', NULL, NULL, NULL, '54@default', '037f3c092ae05f73054d2d2dd0e7d5dc', 
+'rfc2833', NULL, NULL, NULL, '54@default', 
+'037f3c092ae05f73054d2d2dd0e7d5dc', 
 NULL, NULL, 'yes', '', 'all', 'g729;ilbc;gsm;ulaw;alaw', '0', NULL, '0', 
 '0', '', '', NULL, NULL, '1');
