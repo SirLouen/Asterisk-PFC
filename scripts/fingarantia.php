7100042db0f18bfc60f37f02b6b633c2b17dd650
Manuel Camargo <sir.louen@gmail.com>
2012-06-11 22:56:42 +0200

    Little mod

--- scripts/fingarantia.php
+++ scripts/fingarantia.php
@@ -6,16 +6,12 @@ require_once("/var/lib/asterisk/agi-bin/phpagi/phpagi.php");
 function fecha_normal($fecha)
 {
     ereg( "([0-9]{2,4})-([0-9]{1,2})-([0-9]{1,2})", $fecha, $mifecha);
-//    $lafecha=$mifecha[3]."/".$mifecha[2]."/".$mifecha[1];
     $lafecha=$mifecha[3].$mifecha[2].$mifecha[1];
     return $lafecha;
 }
 
 function resta_fecha($fecha_inicial, $fecha_final)
 {
-//    $fecha_inicial = str_replace("/","",$fecha_inicial);
-//    $fecha_final = str_replace("/","",$fecha_final);
-
     ereg( "([0-9]{1,2})([0-9]{1,2})([0-9]{2,4})", $fecha_inicial, $fini);
     ereg( "([0-9]{1,2})([0-9]{1,2})([0-9]{2,4})", $fecha_final, $ffin);
 
